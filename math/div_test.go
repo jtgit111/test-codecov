5876eb194505838c0ff151d7639373670f494031
jiangtao111 <2114181317@qq.com>
2025-01-08 18:50:46 +0800

    add div test

--- math/div_test.go
+++ math/div_test.go
@@ -1,15 +1,15 @@
 package testCodecov
 
-// import "testing"
+import "testing"
 
-// func TestDiv(t *testing.T) {
-//     tests := []struct{a, b, expected int} {
-//         {2, 1, 2},
-//     }
-//     for _, test := range tests {
-//         res := Div(test.a, test.b)
-//         if res != test.expected {
-//             t.Errorf("Div(%d, %d) = %d; want %d", test.a, test.b, res, test.expected)
-//         }
-//     }
-// }
+func TestDiv(t *testing.T) {
+    tests := []struct{a, b, expected int} {
+        {2, 1, 2},
+    }
+    for _, test := range tests {
+        res := Div(test.a, test.b)
+        if res != test.expected {
+            t.Errorf("Div(%d, %d) = %d; want %d", test.a, test.b, res, test.expected)
+        }
+    }
+}
