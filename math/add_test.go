a97765d9ebcd76aca7cfd0e5ef81e2dc9d4bc67f
jiangtao111 <2114181317@qq.com>
2025-01-08 18:48:44 +0800

    add add test

--- math/add_test.go
+++ math/add_test.go
@@ -1,21 +1,21 @@
 package testCodecov
 
-// import "testing"
+import "testing"
 
-// func TestAdd(t *testing.T) {
-//     tests := []struct {
-//         a, b, expected int
-//     }{
-//         {1, 2, 3},
-//         {2, 3, 5},
-//         {-1, 1, 0},
-//         {0, 0, 0},
-//     }
+func TestAdd(t *testing.T) {
+    tests := []struct {
+        a, b, expected int
+    }{
+        {1, 2, 3},
+        {2, 3, 5},
+        {-1, 1, 0},
+        {0, 0, 0},
+    }
 
-//     for _, test := range tests {
-//         result := Add(test.a, test.b)
-//         if result != test.expected {
-//             t.Errorf("Add(%d, %d) = %d; want %d", test.a, test.b, result, test.expected)
-//         }
-//     }
-// }
+    for _, test := range tests {
+        result := Add(test.a, test.b)
+        if result != test.expected {
+            t.Errorf("Add(%d, %d) = %d; want %d", test.a, test.b, result, test.expected)
+        }
+    }
+}
