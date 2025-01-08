22af18e7f8771bd6d76869f1b8856b889e95d8ca
jiangtao111 <2114181317@qq.com>
2025-01-08 19:32:11 +0800

    add sub test

--- math/sub_test.go
+++ math/sub_test.go
@@ -1,16 +1,16 @@
 package testCodecov
 
-// import "testing"
+import "testing"
 
-// func TestSub(t *testing.T) {
-//     tests := []struct{a, b, expected int} {
-//         {2, 1, 1},
-//     }
+func TestSub(t *testing.T) {
+    tests := []struct{a, b, expected int} {
+        {2, 1, 1},
+    }
 
-//     for _, test := range tests {
-//         res := Sub(test.a, test.b)
-//         if res != test.expected {
-//             t.Errorf("Sub(%d, %d) = %d; want %d", test.a, test.b, res, test.expected)
-//         }
-//     }
-// }
+    for _, test := range tests {
+        res := Sub(test.a, test.b)
+        if res != test.expected {
+            t.Errorf("Sub(%d, %d) = %d; want %d", test.a, test.b, res, test.expected)
+        }
+    }
+}
