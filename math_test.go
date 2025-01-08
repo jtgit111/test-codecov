00a2c02c2cdff3f49f2c9ff7e893306cf3ca7854
jiangtao111 <2114181317@qq.com>
2025-01-08 12:05:20 +0800

    add mul func

--- math_test.go
+++ math_test.go
@@ -32,3 +32,15 @@ func TestSub(t *testing.T) {
         }
     }
 }
+
+func TestMul(t *testing.T) {
+    tests := []struct{a, b, expected int} {
+        {2, 1, 2},
+    }
+    for _, test := range tests {
+        res := Mul(test.a, test.b)
+        if res != test.expected {
+            t.Errorf("Mul(%d, %d) = %d; want %d", test.a, test.b, res, test.expected)
+        }
+    }
+}
