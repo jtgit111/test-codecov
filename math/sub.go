f25b2381e25649671b6d97574dbf58ba9da46352
jiangtao111 <2114181317@qq.com>
2025-01-08 20:21:17 +0800

    add subThree1 func and test

--- math/sub.go
+++ math/sub.go
@@ -7,3 +7,7 @@ func Sub(a, b int) int {
 func SubThree(a, b, c int) int {
     return a - b - c
 }
+
+func SubThree1(a, b, c int) int {
+    return a - b - c
+}
--- math/sub_test.go
+++ math/sub_test.go
@@ -14,3 +14,16 @@ func TestSub(t *testing.T) {
         }
     }
 }
+
+func TestSubThree1(t *testing.T) {
+    tests := []struct{a, b, c, expected int} {
+        {2, 1, 1, 0},
+    }
+
+    for _, test := range tests {
+        res := SubThree(test.a, test.b, test.c)
+        if res != test.expected {
+            t.Errorf("Sub(%d, %d, %d) = %d; want %d", test.a, test.b, test.c, res, test.expected)
+        }
+    }
+}
